ce490be96ab8594742a3d5cd100f88ba5d2d430a
Rakshit Bhatt <rakshitbhatt987@gmail.com>
2024-11-17 00:12:30 +0530

    Added EJS view setup and updated package.json dependencies

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
 io.on("connection", (socket) => {
