4c4652dd5c30ae4e332f022035cb85cf3be4aeca
Rakshit Bhatt <rakshitbhatt987@gmail.com>
2024-11-16 22:43:42 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const socketio = require("socket.io");
+const http = require("http");
+const path = require("path");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection", (socket) => {
+  socket.on("send-location", (data) => {
+    io.emit("receive-location", { id: socket.id, ...data });
+  });
+  socket.on("disconnect", () => {
+    io.emit("user-disconnected", socket.id);
+  });
+  console.log("User connected");
+});
+
+app.get("/", function (req, res) {
+  res.render("index");
+});
+
+server.listen(8000, () => {
+  console.log("Server is Running");
+});
